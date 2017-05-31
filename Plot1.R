0112152ed8b4e168f691cbd3026352de326cd4b2
openended111 <openended111@gmail.com>
2017-05-31 00:38:16 -0400

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,10 +1,9 @@
 library("dplyr")
-power <- read.table("household_power_consumption.txt",header=TRUE,sep=";")
+power <- read.table("household_power_consumption.txt",header=TRUE,sep=";",na.strings="?")
 t1 <- as.Date("2007-2-1")
 t2 <- as.Date("2007-2-2")
 powersub <- subset(power,as.Date(power$Date,format="%d/%m/%Y") %in% t1:t2)
-powersub$Global_active_power <- as.numeric(powersub$Global_active_power)/1000
+powersub$Global_active_power <- as.numeric(powersub$Global_active_power)
 with(powersub,hist(powersub$Global_active_power,xlab="Global Active Power (killowatts)",col='red',main="Global Active Power"))
 dev.copy(png,file="plot1.png",width = 480,height = 480)
 dev.off()
-
